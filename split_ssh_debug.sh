cd206a6bb9bc9ae6b4379761e0b2d803971627b2
Amos Thacker-Gwaltney <morphouswatery@gmail.com>
2025-12-21 00:36:11 -0500

    cleaned up the scripts
    Im going to do some more debugging since I trimmed some things.
    Changed the bash scripts to make them more reasonable.
    
    I'm planning on dropping the split-ssh-setup package in favor of holding
    bin files on dom0 which I can place with salt. Its automatic once you
    have the bin files which is enough for me. I don't like the way I wrote
    the split-ssh-setup package or that I even wrote it to begin with; it's
    ugly and bulky.

--- split_ssh_debug.sh
+++ split_ssh_debug.sh
@@ -1,61 +1,77 @@
 # /bin/bash!
 #
 # Script for automated testing of 
-# qubes-split-ssh workspace functionalities
+# qubes-split-ssh workspace
+#
 # Change these if personal values differ:
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
-LOCAL_SCRIPT_PATH="/home/shannel/debug_scripts/split_ssh_vm_build.sh";
-LOCAL_VAULT_STARTUP_SCRIPT_PATH="/home/shannel/debug_scripts/vault_startup.sh";
-QINC_SCRIPT_PATH="/QubesIncoming/dom0/split_ssh_vm_build.sh";
+
+COMPILER_VM="dev"
+COMPILER_VM_USER="user"
+PROJECT_DIR="/home/$COMPILER_VM_USER/projects/rust/qubes-split-ssh"
 
 
-COMPILER_VM="dev";
-COMPILER_VM_USER="user";
-PROJECT_DIR="/home/$COMPILER_VM_USER/projects/rust/qubes-split-ssh";
+VAULT_VM_USER="user"
+VAULT_VM="ssh-vault"
 
 
-VAULT_VM_USER="user";
-VAULT_QINC="/home/$VAULT_VM_USER/QubesIncoming/dom0";
-VAULT_ZIP_PATH="$VAULT_QINC/qss.zip";
-VAULT_SCRIPT_PATH="$VAULT_QINC/split_ssh_vm_build.sh";
-VAULT_STARTUP_SCRIPT_PATH="$VAULT_QINC/vault_startup.sh";
-VAULT_VM="ssh-vault";
-VAULT_PKG_NAME="vault_handler";
+CLIENT_VM_USER="user"
+CLIENT_VM="ssh-client-dvm"
 
-
-CLIENT_VM_USER="user";
-CLIENT_QINC="/home/$CLIENT_VM_USER/QubesIncoming/dom0";
-CLIENT_ZIP_PATH="$CLIENT_QINC/qss.zip";
-CLIENT_SCRIPT_PATH="$CLIENT_QINC/split_ssh_vm_build.sh";
-CLIENT_VM="ssh-client-dvm";
-CLIENT_PKG_NAME="client_handler";
 # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ #
 
-WORKING_DIR="$(mktemp --directory)" || exit 1;
+err_exit() {
+  local ECODE=$?
+  rm -rf $WORKING_DIR &> /dev/null || true;
+  echo "$BASH_COMMAND @: $LINENO"
+  exit $ECODE
+}
 
-qvm-run --pass-io -u $COMPILER_VM_USER $COMPILER_VM "
-cd $PROJECT_DIR &&
+trap err_exit ERR
+
+LOCAL_SCRIPT_PATH=$0
+LOCAL_VAULT_STARTUP_SCRIPT_PATH=$(dirname $0)
+QINC_SCRIPT_PATH="/QubesIncoming/dom0/split_ssh_vm_build.sh"
+
+VAULT_QINC="/home/$VAULT_VM_USER/QubesIncoming/dom0"
+VAULT_ZIP_PATH="$VAULT_QINC/qss.zip"
+VAULT_SCRIPT_PATH="$VAULT_QINC/split_ssh_vm_build.sh"
+VAULT_STARTUP_SCRIPT_PATH="$VAULT_QINC/vault_startup.sh"
+VAULT_PKG_NAME="vault_handler"
+
+CLIENT_SCRIPT_PATH="$CLIENT_QINC/split_ssh_vm_build.sh"
+CLIENT_QINC="/home/$CLIENT_VM_USER/QubesIncoming/dom0"
+CLIENT_ZIP_PATH="$CLIENT_QINC/qss.zip"
+CLIENT_PKG_NAME="client_handler"
+
+
+WORKING_DIR="$(mktemp --directory)"
+
+qvm-run --pass-io -u $COMPILER_VM_USER $COMPILER_VM "\
+cd $PROJECT_DIR && \
 zip -r qss.zip ./* -x 'target/***' '.git/***' '.gitignore' \
-'notes/***' 'split_ssh_debug.sh' 'split_ssh_vm_build.sh' &&
-cat qss.zip" > $WORKING_DIR/qss.zip || exit 2;
-qvm-run $COMPILER_VM "rm -f $PROJECT_DIR/qss.zip" || exit 3;
+'notes/***' 'split_ssh_debug.sh' 'split_ssh_vm_build.sh' && \
+cat qss.zip" > \
+$WORKING_DIR/qss.zip
 
-qvm-run -u $CLIENT_VM_USER $CLIENT_VM "
-rm -f $CLIENT_ZIP_PATH $CLIENT_SCRIPT_PATH" || exit 8; 
-qvm-copy-to-vm $CLIENT_VM $WORKING_DIR/qss.zip || exit 4;
-qvm-copy-to-vm $CLIENT_VM $LOCAL_SCRIPT_PATH || exit 5;
+qvm-run $COMPILER_VM "rm -f $PROJECT_DIR/qss.zip"
 
-qvm-run -u $VAULT_VM_USER  $VAULT_VM "
-rm -f $VAULT_ZIP_PATH $VAULT_SCRIPT_PATH" || exit 9;
-qvm-copy-to-vm $VAULT_VM $WORKING_DIR/qss.zip || exit 6;
-qvm-copy-to-vm $VAULT_VM $LOCAL_SCRIPT_PATH || exit 7; 
+qvm-run -u $CLIENT_VM_USER $CLIENT_VM "\
+rm -f $CLIENT_ZIP_PATH $CLIENT_SCRIPT_PATH"
 
-qvm-run -u root --pass-io $CLIENT_VM "
-$CLIENT_SCRIPT_PATH $CLIENT_PKG_NAME $CLIENT_VM_USER 0" || exit 8;
+qvm-copy-to-vm $CLIENT_VM $WORKING_DIR/qss.zip
+qvm-copy-to-vm $CLIENT_VM $LOCAL_SCRIPT_PATH
 
-qvm-run -u root --pass-io $VAULT_VM "
-$VAULT_SCRIPT_PATH $VAULT_PKG_NAME $VAULT_VM_USER 1" || exit 9;
+qvm-run -u $VAULT_VM_USER  $VAULT_VM "\
+rm -f $VAULT_ZIP_PATH $VAULT_SCRIPT_PATH"
 
-rm -rf $WORKING_DIR &> /dev/null || exit 10; 
+qvm-copy-to-vm $VAULT_VM $WORKING_DIR/qss.zip
+qvm-copy-to-vm $VAULT_VM $LOCAL_SCRIPT_PATH
 
-exit 0;
+qvm-run -u root --pass-io $CLIENT_VM "\
+$CLIENT_SCRIPT_PATH $CLIENT_PKG_NAME $CLIENT_VM_USER 0"
+
+qvm-run -u root --pass-io $VAULT_VM "\
+$VAULT_SCRIPT_PATH $VAULT_PKG_NAME $VAULT_VM_USER 1"
+
+rm -rf $WORKING_DIR &> /dev/null
--- split_ssh_vm_build.sh
+++ split_ssh_vm_build.sh
@@ -5,34 +5,43 @@
 # # # # # # # # # # # # # # # 
 
 # Script Arguments #
-PKG_NAME="$1";
-USER_U="$2";
-IS_VAULT="$3"; 
+PKG_NAME="$1"
+USER_U="$2"
+IS_VAULT="$3"
 # ~~~~~~~~~~~~~~~~ #
 
-HOME_U="/home/$USER_U";
-QINC_DIR="$HOME_U/QubesIncoming/dom0";
-SELF_PATH="$QINC_DIR/split_ssh_vm_build.sh";
-ZIP_PATH="$QINC_DIR/qss.zip";
-PROJ_DIR="$HOME_U/qss";
-PROJ_MANIFEST="$PROJ_DIR/$PKG_NAME/Cargo.toml";
-AOUT="$PROJ_DIR/target/debug/$PKG_NAME";
-LOGS_DIR="$HOME_U/.local/state/split-ssh";
-VAULT_RPC_PATH="/etc/qubes-rpc/qubes.SplitSSHAgent";
+err_exit() {
+  echo "$BASH_COMMAND @: $LINENO"
+  exit $?
+}
 
-if [ -d $PROJ_DIR ]; then
-  rm -rf $PROJ_DIR; fi  
+trap err_exit ERR
 
-if [ -d $LOGS_DIR ]; then
-  rm -rf $LOGS_DIR; fi
+HOME_U="/home/$USER_U"
+QINC_DIR="$HOME_U/QubesIncoming/dom0"
+ZIP_PATH="$QINC_DIR/qss.zip"
+PROJ_DIR="$HOME_U/qss"
+PROJ_MANIFEST="$PROJ_DIR/$PKG_NAME/Cargo.toml"
+AOUT="$PROJ_DIR/target/debug/$PKG_NAME"
+LOGS_DIR="$HOME_U/.local/state/split-ssh"
+VAULT_RPC_PATH="/etc/qubes-rpc/qubes.SplitSSHAgent"
 
-mkdir $PROJ_DIR || exit 3;
-unzip $ZIP_PATH -d $PROJ_DIR || true;
-cargo build --manifest-path $PROJ_MANIFEST || exit 2;
-chmod 755 $AOUT || exit 4
+if [[ -d $PROJ_DIR ]]; then
+  rm -rf $PROJ_DIR 
+fi  
+
+if [[ -d $LOGS_DIR ]]; then
+  rm -rf $LOGS_DIR 
+fi
+
+mkdir $PROJ_DIR
+unzip $ZIP_PATH -d $PROJ_DIR
+cargo build --manifest-path $PROJ_MANIFEST
+chmod 755 $AOUT
 
 if [ $IS_VAULT == 1 ]; then 
-chown root:root $AOUT || exit 5;
-mv $AOUT $VAULT_RPC_PATH || exit 6; fi
+  chown $USER_U:$USER_U $AOUT
+  mv $AOUT $VAULT_RPC_PATH 
+fi
 
-rm -f $SELF_PATH || exit ;
+rm -f $0
